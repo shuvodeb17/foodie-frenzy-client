5c5e2c7f8f297ddc5b80df5db22af3da5e7b019d
Shuvo Deb <shuvodeb17014@gmail.com>
2023-05-02 15:31:52 +0600

    chef-recipe page add react rating

--- src/components/ChefRecipes/ChefRecipes.jsx
+++ src/components/ChefRecipes/ChefRecipes.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react';
 import Footer from '../Footer/Footer';
 import { useLoaderData } from 'react-router-dom';
 import './ChefRecipes.css';
+import { HandThumbUpIcon } from '@heroicons/react/24/solid';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const ChefRecipes = () => {
     const chefRecipes = useLoaderData();
     console.log(chefRecipes);
-    const { picture, name, description, likes, numbersOfRecipes, experience, recipeName } = chefRecipes;
+    const [rating, setRating] = useState(0)
+    const { picture, name, description, likes, numbersOfRecipes, experience, recipeName, ingredients, cookingMethod, ratings } = chefRecipes;
 
 
 
@@ -18,22 +22,44 @@ const ChefRecipes = () => {
                         <div className='chef-main bg-[#0E1317] p-5 '>
                             <img className='chef-recipe-img rounded-lg' src={picture} alt="" />
                             <div>
-                                <h2 className="text-3xl font-bold mt-2">Name: {name}</h2>
-                                <p>Description: {description}</p>
-                                <div>
+                                <h2 className="text-3xl font-bold mt-2 mb-3">Name: {name}</h2>
+                                <p className='mb-3'><span className='font-bold'>Description:</span> {description}</p>
+                                <p> <span className='font-bold'>Number of Recipes:</span> {numbersOfRecipes}</p>
+                                <p className='mb-3'> <span className='font-bold'>Experience: </span>{experience}</p>
+                                <div className='flex mb-3'>
+                                    <HandThumbUpIcon className="me-3 h-6 w-6 text-blue-500" />
                                     <span>{likes}</span>
                                 </div>
-                                <p>Number of Recipes: {numbersOfRecipes}</p>
-                                <p>Experience: {experience}</p>
                             </div>
-                            
-                            <div>
-                                <div>
+
+                            <div className='grid grid-cols-2 recipe-page-multiple mt-5 p-3'>
+                                <div className='recipe-page-left'>
                                     <h2 className="text-2xl font-bold mt-3">Recipe Name</h2>
                                     {
                                         recipeName.map(r => <p> {r}</p>)
                                     }
                                 </div>
+                                <div className='recipe-page-right'>
+                                    <h2 className="text-2xl font-bold mt-3">Ingredients</h2>
+                                    {
+                                        ingredients.map(ingredient => <p>
+                                            {ingredient}
+                                        </p>)
+                                    }
+                                </div>
+                            </div>
+                            <div className='mt-3'>
+                                <h2 className="">
+                                    <span className='font-bold'>Cooking Method :</span>
+                                    <span> {cookingMethod}</span>
+                                </h2>
+
+                                {/* ratings */}
+                                <h2 className='mt-3'>
+                                    <span className="font-bold">Ratings : </span>
+                                    {ratings}
+                                </h2>
+                                <Rating style={{ maxWidth: 100 }} value={ratings} readOnly />
                             </div>
                         </div>
                     </div>
